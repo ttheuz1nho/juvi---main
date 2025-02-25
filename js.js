878ee19bcecb2b9c09d593a524a9da0c40c32783
malmeida <m.almeidadigital@outlook.com>
2025-02-25 21:44:33 +0100

    Consertando bugs

--- js.js
+++ js.js
@@ -2,3 +2,13 @@ document.getElementById('mobile_btn').addEventListener('click', function() {
     const navList = document.getElementById('nav_list');
     navList.classList.toggle('active');
 });
+
+// Fecha o menu ao clicar fora dele
+document.addEventListener('click', function(event) {
+    const navList = document.getElementById('nav_list');
+    const mobileBtn = document.getElementById('mobile_btn');
+
+    if (!navList.contains(event.target) && !mobileBtn.contains(event.target)) {
+        navList.classList.remove('active');
+    }
+});
